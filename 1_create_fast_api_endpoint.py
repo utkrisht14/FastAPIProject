06e30d761498dca0eff5e01aa6be2b6e8cfa4f35
Utkrisht Mallick <utkrisht14@gmail.com>
2026-05-14 19:24:26 +0200

    Put and Delete request added.

--- 1_create_fast_api_endpoint.py
+++ 1_create_fast_api_endpoint.py
@@ -50,3 +50,19 @@ async def read_author_category_by_query(book_author: str, category: str):
 async def create_book(new_book=Body()):
     BOOKS.append(new_book)
     return new_book
+
+
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i]["title"].casefold() == updated_book["title"].casefold():
+            BOOKS[i] = updated_book
+    return updated_book
+
+
+@app.delete("/books/delete_book")
+async def delete_book(book_title: str):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get("title").casefold() == book_title.casefold():
+            BOOKS.pop(i)
+            break
