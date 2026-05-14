caeaed7dbd7c2ec9ca16330ab26c50ae5368b5d2
Utkrisht Mallick <utkrisht14@gmail.com>
2026-05-14 17:45:16 +0200

    Books name added.

--- 1_create_fast_api_endpoint.py
+++ 1_create_fast_api_endpoint.py
@@ -2,6 +2,15 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/")
+BOOKS = [
+{"title": "A Brief History of Time", "author": "Stephen Hawking", "category": "Science"},
+    {"title": "Sapiens", "author": "Yuval Noah Harari", "category": "History"},
+    {"title": "The Alchemist", "author": "Paulo Coelho", "category": "Fiction"},
+    {"title": "Atomic Habits", "author": "James Clear", "category": "Self-Help"},
+    {"title": "Clean Code", "author": "Robert C. Martin", "category": "Programming"}
+]
+
+
+@app.get("/api-endpoint")
 async def first_api():
-    return {"message": "Hello World"}
+    return BOOKS
