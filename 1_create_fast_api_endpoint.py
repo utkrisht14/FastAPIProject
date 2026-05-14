03c98ba709af66682940d258338c20f6fee0990c
Utkrisht Mallick <utkrisht14@gmail.com>
2026-05-14 19:10:47 +0200

    Post request completed.

--- 1_create_fast_api_endpoint.py
+++ 1_create_fast_api_endpoint.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import Body, FastAPI
 
 app = FastAPI()
 
@@ -44,3 +44,9 @@ async def read_author_category_by_query(book_author: str, category: str):
             books_to_return.append(book)
 
     return books_to_return
+
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+    return new_book
