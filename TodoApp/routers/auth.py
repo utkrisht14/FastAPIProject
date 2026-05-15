448c26a41afc8f0e9fc2dbc0bddf761cf2b47d5d
Utkrisht Mallick <utkrisht14@gmail.com>
2026-05-15 19:57:01 +0200

    Hashing the password

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,7 +1,35 @@
 from fastapi import APIRouter
-
+from pydantic import BaseModel
+from models import Users
+from passlib.context import CryptContext
 
 router = APIRouter()
+
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+
+@router.post("/auth")
+async def create_user(create_user_request: CreateUserRequest):
+    create_user_model = Users(
+        email = create_user_request.email,
+        username = create_user_request.username,
+        first_name= create_user_request.first_name,
+        last_name= create_user_request.last_name,
+        hashed_password= bcrypt_context.hash(create_user_request.password),
+        role= create_user_request.role,
+        is_active= True
+    )
+
+    return create_user_model
+
 
 
 
