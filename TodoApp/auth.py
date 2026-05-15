a1f93567982a058cae4e514c55ea8e6e9bfbdb48
Utkrisht Mallick <utkrisht14@gmail.com>
2026-05-15 18:53:43 +0200

    Auth file created

---
+++ TodoApp/auth.py
@@ -0,0 +1,10 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+@app.get("/auth/")
+async def get_user():
+    return {"user": "authenticated"}
+
+
--- TodoApp/main.py
+++ TodoApp/main.py
@@ -106,3 +106,15 @@ async def update_todo(
 
     # Commit changes permanently into the database
     db.commit()
+
+
+
+# Delete the app endpoint
+@app.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_todo(db: db_dependency, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail="Todo not found.")
+    db.query(Todos).filter(Todos.id == todo_id).delete()
+    db.commit()
+
