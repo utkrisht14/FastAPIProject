370d78dc4197c0ae5150f32b9a8cd57992cf93ca
Utkrisht Mallick <utkrisht14@gmail.com>
2026-05-15 12:21:43 +0200

    Functionality of published date added.

--- 2_pro_books.py
+++ 2_pro_books.py
@@ -10,21 +10,26 @@ class Book:
     author: str
     description:str
     rating:int
+    published_date: int
 
-    def __init__(self, id:int, title:str, author: str, description:str, rating:int):
+    def __init__(self, id, title, author, description, rating, published_date):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_date = published_date
 
 
+# Pydantic model for the book request used for validation
 class BookRequest(BaseModel):
     id: Optional[int] = Field(description="ID is not needed on create", default =None)
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(ge=1, le=5)
+    published_date: int = Field(description="Year of publication", default=None)
+
 
     model_config = {
         "json_schema_extra": {
@@ -39,12 +44,12 @@ class BookRequest(BaseModel):
 
 
 BOOKS = [
-    Book(1, "The Hobbit", "", "A fellowship of men and dwarves", 4),
-    Book(2, "The Lord of the Rings", "", "A fellowship of men and dwarves", 5),
-    Book(3, "Harry Potter and the Sorcerer's Stone", "J.K. Rowling", "A young wizard begins his magical journey at Hogwarts", 5),
-    Book(4, "1984", "George Orwell", "A dystopian world ruled by surveillance and control", 4),
-    Book(5, "To Kill a Mockingbird", "Harper Lee", "A story of justice and racial inequality in a small town", 5),
-    Book(6, "The Great Gatsby", "F. Scott Fitzgerald", "A young man's adventures in a world of magic and alchemy", 4),
+    Book(1, "The Hobbit", "J.R.R. Tolkien", "A fellowship of men and dwarves", 4, 1937),
+    Book(2, "The Lord of the Rings", "J.R.R. Tolkien", "A fellowship of men and dwarves", 5, 1954),
+    Book(3, "Harry Potter and the Sorcerer's Stone", "J.K. Rowling","A young wizard begins his magical journey at Hogwarts", 5, 1997),
+    Book(4, "1984", "George Orwell", "A dystopian world ruled by surveillance and control", 4, 1949),
+    Book(5, "To Kill a Mockingbird", "Harper Lee", "A story of justice and racial inequality in a small town", 5, 1960),
+    Book(6, "The Great Gatsby", "F. Scott Fitzgerald", "A young man's adventures in a world of magic and alchemy", 4,1925),
 ]
 
 
@@ -72,14 +77,26 @@ async def fetch_book_by_rating(rating: int):
     return books_to_return
 
 
+# Function that get book by publish date
+async def find_by_publish_date(published_date: int)
+    books_to_return = []
+    for book in BOOKS:
+        if book.published_date == published_date:
+            books_to_return.append(book)
+    return books_to_return
 
 
+
+# Function that creates a new book
 @app.post("/create_book")
 async def create_book(book_request: BookRequest):
     new_book = Book(**book_request.model_dump())  # book_request.model_dump() -> converts Pydantic object → dictionary.
     BOOKS.append(find_book_id(new_book))
     return book_request
 
+
+# Function that assigns an id to the book
+@app.get("/books/id")
 def find_book_id(book: Book):
     book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
     return book
@@ -91,3 +108,12 @@ async def update_book(book: BookRequest):
     for i in range(len(BOOKS)):
         if BOOKS[i].id == book.id:
             BOOKS[i] = book
+
+# Delete the book by id
+@app.delete("/books/{book_id")
+async def delete_book(book_id: int):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            break
+    return {"message": "Book deleted"}
