e858ffd4a1603dfc80796b7293e3c622fa5c1ced
Utkrisht Mallick <utkrisht14@gmail.com>
2026-05-15 12:11:45 +0200

    PUT request method added.

--- 2_pro_books.py
+++ 2_pro_books.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Body
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Optional
 
 app = FastAPI()
 
@@ -19,14 +20,22 @@ class Book:
 
 
 class BookRequest(BaseModel):
-    id: int
-    title: str
-    author: str
-    description: str
-    rating: int
+    id: Optional[int] = Field(description="ID is not needed on create", default =None)
+    title: str = Field(min_length=3)
+    author: str = Field(min_length=1)
+    description: str = Field(min_length=1, max_length=100)
+    rating: int = Field(ge=1, le=5)
 
-
-
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "The Hobbit",
+                "author": "J.R.R. Tolkien",
+                "description": "A fellowship of men and dwarves",
+                "rating": 4,
+            }
+        }
+    }
 
 
 BOOKS = [
@@ -44,8 +53,41 @@ async def read_all_books():
     return BOOKS
 
 
+# Function that fetches the book by the id
+@app.get("/books/{book_id}")
+async def read_book(book_id: int):
+    for book in BOOKS:
+        if book.id == book_id:
+            return book
+    return {"message": "Book not found"}
+
+
+# Function that fetches the book by rating
+@app.get("/books/")
+async def fetch_book_by_rating(rating: int):
+    books_to_return = []
+    for book in BOOKS:
+        if book.rating == rating:
+            books_to_return.append(book)
+    return books_to_return
+
+
+
+
 @app.post("/create_book")
-async def create_book(book_request: BookRequest ):
-    new_book = Book(**book_request.model_dump())
-    BOOKS.append(new_book)
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())  # book_request.model_dump() -> converts Pydantic object → dictionary.
+    BOOKS.append(find_book_id(new_book))
     return book_request
+
+def find_book_id(book: Book):
+    book.id = 1 if len(BOOKS) == 0 else BOOKS[-1].id + 1
+    return book
+
+
+# Update book with the book request
+@app.put("/books/update_book")
+async def update_book(book: BookRequest):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            BOOKS[i] = book
